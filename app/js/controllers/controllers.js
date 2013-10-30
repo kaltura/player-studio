094d6362ac076626943fbe0796b200e33b122876
Nadav Sinai <ns@nadavsinai.com>
2013-10-30 10:32:18 +0200

    menu on the way

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -3,17 +3,20 @@
 /* Controllers */
 //
 angular.module('KMC.controllers', [])
-        .controller('ModalInstanceCtrl',
-                function($scope, $modalInstance, settings,$compile) {
-                    $scope.title = '';
-                    $scope.message = '';
-                    $scope.buttons = [{result: false, label: 'Cancel', cssClass: 'btn-default'}, {result: true, label: 'OK', cssClass: 'btn-primary'}];
-                    angular.extend($scope, settings);
-                    $scope.close = function(result) {
-                        $modalInstance.close(result);
-                    };
-                    $scope.cancel = function() {
-                        $modalInstance.dismiss('cancel');
-                    };
-                }
-        );
+    .controller('ModalInstanceCtrl',
+    function ($scope, $modalInstance, settings, $compile) {
+        $scope.title = '';
+        $scope.message = '';
+        $scope.buttons = [
+            {result: false, label: 'Cancel', cssClass: 'btn-default'},
+            {result: true, label: 'OK', cssClass: 'btn-primary'}
+        ];
+        angular.extend($scope, settings);
+        $scope.close = function (result) {
+            $modalInstance.close(result);
+        };
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    }
+);
