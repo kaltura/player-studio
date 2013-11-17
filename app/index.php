2fb98816458346b091dbd4bb7130df99ed6daa8a
Nadav Sinai <ns@nadavsinai.com>
2013-11-17 12:25:18 +0200

    this bullshit is needed for windows support of the PHP base tag injection.

--- app/index.php
+++ app/index.php
@@ -1,63 +1,67 @@
 <!doctype html>
 <html lang="en" ng-app="KMCModule">
-    <head>
-        <meta charset="utf-8">
-		<base id="baseHref" href="<?php echo dirname($_SERVER['REQUEST_URI'])?>"/>
-        <script type="text/javascript" src="bower_components/jquery/jquery.js"></script>
-        <!--change this base tag to the root of your app URL-->
-        <title>Player Studio - JS Version</title>
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="css/app.css"/>
-        <link rel="stylesheet" href="css/menu.css"/>
-        <link rel="stylesheet" href="css/icons.css"/>
-        <link rel='stylesheet' href='lib/colorpicker/css/colorpicker.css'/>
-        <link rel='stylesheet' href='lib/spinedit/css/bootstrap-spinedit.css'/>
-        <!--[if gte IE 9]>
-        <style type="text/css">
-            .gradient {
-                filter: none;
-            }
-        </style>
-        <![endif]-->
-        <!-- TODO move to ini file -->
-        <script type="text/javascript" src="http://kgit.html5video.org/tags/v2.0.0.rc7/mwEmbedLoader.php"></script>
-        <link rel="stylesheet" href="bower_components/select2/select2.css">
-        <link rel="stylesheet" href="bower_components/prettycheckable/dist/prettyCheckable.css">
-        <script type="text/javascript" src="bower_components/select2/select2.js"></script>
-        <!--1.07-->
-        <!--<script src="lib/angular/angular.js"></script>-->
-        <!--1.2-->
-        <script type="text/javascript" src="bower_components/angular/angular.js"></script>
-        <script type="text/javascript" src="bower_components/angular-route/angular-route.js"></script>
-        <script type="text/javascript" src="bower_components/angular-ui-select2/src/select2.js"></script>
-        <script src="lib/sprintf.js"></script>
-        <script src="lib/localize.js"></script>
-        <script src="lib/spin.min.js"></script>
-        <script src="lib/bootstrap-ui/ui-bootstrap-0.6.0.js"></script>
-        <script src="lib/spinedit/js/bootstrap-spinedit.js"></script>
-        <script src="lib/colorpicker/js/bootstrap-colorpicker-module.js"></script>
-        <script src="lib/localStorage/angular-local-storage.js"></script>
-        <script src="lib/jquery.timeago.js"></script>
-        <script src="lib/jquery.animate-colors-min.js"></script>
-        <script src="js/app.js"></script>
-        <script src="js/menu.js"></script>
-        <script src="js/filters.js"></script>
-        <script src="js/controllers/controllers.js"></script>
-        <script src="js/directives.js"></script>
-        <script src="js/services/services.js"></script>
-        <script src="js/controllers/playerListCtrl.js"></script>
-        <script src="js/controllers/playerEditCtrl.js"></script>
-        <script src="js/controllers/LoginCtrl.js"></script>
-        <script src="js/controllers/playerCreateCtrl.js"></script>
+<head>
+    <meta charset="utf-8">
+    <base id="baseHref" href="<?php
+    $dir = dirname($_SERVER['REQUEST_URI']);
+    $dir = str_replace('\\', '/', $dir);
+    echo $dir;
+    ?>"/>
+    <script type="text/javascript" src="bower_components/jquery/jquery.js"></script>
+    <!--change this base tag to the root of your app URL-->
+    <title>Player Studio - JS Version</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/app.css"/>
+    <link rel="stylesheet" href="css/menu.css"/>
+    <link rel="stylesheet" href="css/icons.css"/>
+    <link rel='stylesheet' href='lib/colorpicker/css/colorpicker.css'/>
+    <link rel='stylesheet' href='lib/spinedit/css/bootstrap-spinedit.css'/>
+    <!--[if gte IE 9]>
+    <style type="text/css">
+        .gradient {
+            filter: none;
+        }
+    </style>
+    <![endif]-->
+    <!-- TODO move to ini file -->
+    <script type="text/javascript" src="http://kgit.html5video.org/tags/v2.0.0.rc7/mwEmbedLoader.php"></script>
+    <link rel="stylesheet" href="bower_components/select2/select2.css">
+    <link rel="stylesheet" href="bower_components/prettycheckable/dist/prettyCheckable.css">
+    <script type="text/javascript" src="bower_components/select2/select2.js"></script>
+    <!--1.07-->
+    <!--<script src="lib/angular/angular.js"></script>-->
+    <!--1.2-->
+    <script type="text/javascript" src="bower_components/angular/angular.js"></script>
+    <script type="text/javascript" src="bower_components/angular-route/angular-route.js"></script>
+    <script type="text/javascript" src="bower_components/angular-ui-select2/src/select2.js"></script>
+    <script src="lib/sprintf.js"></script>
+    <script src="lib/localize.js"></script>
+    <script src="lib/spin.min.js"></script>
+    <script src="lib/bootstrap-ui/ui-bootstrap-0.6.0.js"></script>
+    <script src="lib/spinedit/js/bootstrap-spinedit.js"></script>
+    <script src="lib/colorpicker/js/bootstrap-colorpicker-module.js"></script>
+    <script src="lib/localStorage/angular-local-storage.js"></script>
+    <script src="lib/jquery.timeago.js"></script>
+    <script src="lib/jquery.animate-colors-min.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="js/filters.js"></script>
+    <script src="js/controllers/controllers.js"></script>
+    <script src="js/directives.js"></script>
+    <script src="js/services/services.js"></script>
+    <script src="js/controllers/playerListCtrl.js"></script>
+    <script src="js/controllers/playerEditCtrl.js"></script>
+    <script src="js/controllers/LoginCtrl.js"></script>
+    <script src="js/controllers/playerCreateCtrl.js"></script>
 
-    </head>
-    <body class="scroller">
-        <loading-widget></loading-widget>
-        <section class="relative" ng-view></section>
-    </body>
+</head>
+<body class="scroller">
+<loading-widget></loading-widget>
+<section class="relative" ng-view></section>
+</body>
 </html>
