5519a97310bf5d276cb964e4fc6a13e50f9ff487
Nadav Sinai <ns@nadavsinai.com>
2013-11-24 15:56:13 +0200

    added the player to edit screen select2 update

--- app/js/controllers/playerEditCtrl.js
+++ app/js/controllers/playerEditCtrl.js
@@ -3,8 +3,10 @@
 /* Controllers */
 
 KMCModule.controller('PlayerEditCtrl',
-    ['$scope', 'PlayerData', '$routeParams', '$filter', 'menuSvc', 'PlayerService', 'apiService', 'userEntries',
-        function($scope, PlayerData, $routeParams, $filter, menuSvc, PlayerService, apiService, userEntries) {
+    ['$scope', 'PlayerData', '$routeParams', '$filter', 'menuSvc', 'PlayerService', 'apiService', 'userEntries', 'localStorageService',
+        function($scope, PlayerData, $routeParams, $filter, menuSvc, PlayerService, apiService, userEntries, localStorageService) {
+            // $("head").append('<script src="http://cdnapi.kaltura.com/p/1405721/sp/140572100/embedIframeJs/uiconf_id/' + 21224102 + '/partner_id/' + '1405721' + '"></script>');
+            $scope.ks = localStorageService.get('ks');
             $scope.playerId = $routeParams.id;
             $scope.title = $filter('i18n')('Edit player');
             $scope.data = PlayerData.data;
@@ -22,7 +24,26 @@ KMCModule.controller('PlayerEditCtrl',
                     return  userEntriesList;
                 });
             }
-
+            $scope.previewEntry = ($scope.data.settings.basicDisplay.previewentry) ? $scope.data.settings.basicDisplay.previewentry.id : '0_ji4qh61l';
+            $scope.$watch('data.settings.basicDisplay.previewentry', function(newVal, oldVal) {
+                if (newVal != oldVal) {
+                    $scope.previewEntry = newVal.id;
+                    $scope.renderPlayer();
+                }
+            })
+            $scope.renderPlayer = function() {
+                kWidget.embed({
+                    "targetId": "kplayer",
+                    "wid": "_" + $scope.data.partnerId,
+                    "uiconf_id": $scope.data.id,
+                    "flashvars": {},
+                    "cache_st": 1385293901,
+                    "entry_id": $scope.previewEntry
+                });
+            }
+            $(document).ready(function() {
+                $scope.renderPlayer();
+            });
 
         }
     ])
