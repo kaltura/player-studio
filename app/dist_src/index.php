3c7aafb78e71fce22611bf2178d61492552e4254
Nadav Sinai <ns@nadavsinai.com>
2013-12-24 18:17:16 +0200

    add local bootstrap and add to grunt css and js

--- app/dist_src/index.php
+++ app/dist_src/index.php
@@ -24,12 +24,6 @@
     <script type="text/javascript" src="bower_components/jquery/jquery.min.js"></script>
     <!--<![endif]-->
     <title>Player Studio - JS Version</title>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-    <!-- Optional theme -->
-    <!--    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">-->
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
     <!--[if gte IE 9]>
     <style type="text/css">
         .gradient {
