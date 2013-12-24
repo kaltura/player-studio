70724fd3a1490b80b4ac2267ad1b4a7412df952d
Nadav Sinai <ns@nadavsinai.com>
2013-12-24 22:32:19 +0200

    some minor order changes and add views to templatecache

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
         ],
         preprocessors: {
             '**/*.coffee': 'coffee',
-            'app/template/**/*.html': 'ng-html2js'
+            'app/template/**/*.html': 'ng-html2js',
+            'app/view/**/*.html': 'ng-html2js'
         },
         ngHtml2JsPreprocessor: {
             stripPrefix: 'app/',
@@ -20,17 +21,17 @@ module.exports = function (config) {
         files: [
             'app/bower_components/jquery/jquery.min.js',
             'app/bower_components/angular/angular.js',
+            'app/view/**/*.html',
+            'app/template/**/*.html',
             'app/bower_components/angular-*/angular-*.min.js',
             'app/bower_components/angular-mocks/angular-mocks.js',
             'app/lib/angular-ui-bootstrap/ui-bootstrap-tpls-0.7.0.min.js',
-            'app/js/**/*.js',
             'app/lib/colorpicker/js/bootstrap-colorpicker-module.js',
+            'app/lib/jquery.timeago.js',
             'app/bower_components/angular-ui-sortable/src/sortable.js',
             'app/bower_components/angular-ui-select2/src/select2.js',
-            'test/unit/**/*.js',
-            'app/lib/jquery.timeago.js',
-            'app/template/**/*.html',
-            'app/view/**/*.html'
+            'app/js/**/*.js',
+            'test/unit/**/*.js'
         ],
 
         autoWatch: true,
