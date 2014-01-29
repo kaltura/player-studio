d1066181e339539dd3799e0ad4c4e90f8070d176
Nadav Sinai <ns@nadavsinai.com>
2014-01-29 12:05:52 +0200

    seperate modelCheckbox

--- app/js/directives.js
+++ app/js/directives.js
@@ -658,39 +658,6 @@ DirectivesModule.directive('infoAction', ['menuSvc', function(menuSvc) {
         templateUrl: 'template/formcontrols/infoAction.html'
     };
 }]);
-DirectivesModule.directive('prettycheckbox', function() {
-    return {
-        restrict: 'AC',
-        require: 'ngModel',
-        template: '<a data-ng-click="check()"></a>',
-        link: function(scope, $element, iAttr, ngController) {
-            var clickHandler = $($element).find('a');
-            ngController.$render = function() {
-                if (ngController.$viewValue) {
-                    clickHandler.addClass('checked');
-                }
-                else
-                    ngController.$viewValue = false;
-            };
-            scope.check = function() {
-                ngController.$setViewValue(!ngController.$viewValue);
-            };
-
-            var formatter = function() {
-                if (ngController.$viewValue) {
-                    clickHandler.addClass('checked');
-                }
-                else {
-                    clickHandler.removeClass('checked');
-                }
-            };
-            ngController.$viewChangeListeners.push(formatter);
-            if (scope['require']) {
-                ngController.$setValidity('required', true);
-            }
-        }
-    };
-});
 DirectivesModule.directive('prettyRadio', ['$rootScope', function($rootScope) {
     return {
         restrict: 'AC',
@@ -749,41 +716,6 @@ DirectivesModule.directive('prettyRadio', ['$rootScope', function($rootScope) {
         }
     };
 }]);
-DirectivesModule.directive('modelCheckbox', function() {
-    return {
-        restrict: 'EA',
-        templateUrl: 'template/formcontrols/modelCheckbox.html',
-        require: '?playerRefresh',
-        replace: true,
-        compile: function(tElement, tAttr) {
-            if (tAttr['endline'] == 'true') {
-                tElement.append('<hr/>');
-            }
-            return function($scope, $element, $attrs, playerRefreshCnt) {
-                if (playerRefreshCnt) {
-                    if ($attrs['playerRefresh'] != 'boolean')
-                        $element.attr('player-refresh', 'boolean');
-                    playerRefreshCnt.setUpdateFunction(function(pRscope) { // scope here is pRscope.
-                        pRscope.controlUpdateAllowed = true; // checkbox doesn't need a control watcher, the model is enough.
-                    });
-                }
-            };
-        },
-        controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
-            if ($scope.model === '' || typeof $scope.model == 'undefined') {
-                if ($attrs.initvalue === 'true')
-                    $scope.model = true;
-            }
-        }],
-        scope: {
-            label: '@',
-            helpnote: '@',
-            model: '=',
-            'strModel': '@model',
-            'require': '@'
-        }
-    };
-});
 DirectivesModule.directive('divider', [function() {
     return {
         replace: true,
---
+++ app/js/directives/modelCheckbox.js
@@ -0,0 +1,68 @@
+'use strict';
+var DirectivesModule = angular.module('KMC.directives');
+DirectivesModule.directive('modelCheckbox',function() {
+    return {
+        restrict: 'EA',
+        templateUrl: 'template/formcontrols/modelCheckbox.html',
+        require: '?playerRefresh',
+        replace: true,
+        compile: function(tElement, tAttr) {
+            if (tAttr['endline'] == 'true') {
+                tElement.append('<hr/>');
+            }
+            return function($scope, $element, $attrs, playerRefreshCnt) {
+                if (playerRefreshCnt) {
+                    if ($attrs['playerRefresh'] != 'boolean')
+                        $element.attr('player-refresh', 'boolean');
+                    playerRefreshCnt.setUpdateFunction(function(pRscope) { // scope here is pRscope.
+                        pRscope.controlUpdateAllowed = true; // checkbox doesn't need a control watcher, the model is enough.
+                    });
+                }
+            };
+        },
+        controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
+            if ($scope.model === '' || typeof $scope.model == 'undefined') {
+                if ($attrs.initvalue === 'true')
+                    $scope.model = true;
+            }
+        }],
+        scope: {
+            label: '@',
+            helpnote: '@',
+            model: '=',
+            'require': '@'
+        }
+    };
+}).directive('prettycheckbox', function() {
+    return {
+        restrict: 'AC',
+        require: 'ngModel',
+        template: '<a data-ng-click="check()"></a>',
+        link: function(scope, $element, iAttr, ngController) {
+            var clickHandler = $($element).find('a');
+            ngController.$render = function() {
+                if (ngController.$viewValue) {
+                    clickHandler.addClass('checked');
+                }
+                else
+                    ngController.$viewValue = false;
+            };
+            scope.check = function() {
+                ngController.$setViewValue(!ngController.$viewValue);
+            };
+
+            var formatter = function() {
+                if (ngController.$viewValue) {
+                    clickHandler.addClass('checked');
+                }
+                else {
+                    clickHandler.removeClass('checked');
+                }
+            };
+            ngController.$viewChangeListeners.push(formatter);
+            if (scope['require']) {
+                ngController.$setValidity('required', true);
+            }
+        }
+    };
+});
