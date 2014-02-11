50e68573e6e597cee3b0f19d7e7dfa6de9b4b3a5
amirch1 <amir.chervinsky@kaltura.com>
2014-02-11 11:41:19 +0200

    some cleanup on cntrl and updated UI on edit.html

--- app/js/controllers/playerEditCtrl.js
+++ app/js/controllers/playerEditCtrl.js
@@ -5,10 +5,8 @@
 angular.module('KMCModule').controller('PlayerEditCtrl',
     ['$scope', 'PlayerData', '$routeParams', '$filter', 'menuSvc', 'PlayerService', 'apiService', 'localStorageService',
         function($scope, PlayerData, $routeParams, $filter, menuSvc, PlayerService, apiService, localStorageService) {
-            $scope.ks = localStorageService.get('ks');
             $scope.playerId = PlayerData.id;
             $scope.newPlayer = !$routeParams.id;
-            $scope.title = ($routeParams.id) ? $filter('i18n')('Edit player') : $filter('i18n')('New  player');
             $scope.data = PlayerData;
             $scope.debug = $routeParams.debug;
             $scope.getDebugInfo = function(partial) {
@@ -28,14 +26,14 @@ angular.module('KMCModule').controller('PlayerEditCtrl',
             $scope.settings = {};
 
 // set tags
-            $scope.tags = [];
-// all of the next block is just to show how to push into the tags autocomplete/dropdown the list of available tags should be loaded this way instead,
-// the model tags of the player are actually set properly from the ng-model of the tags directive and are not needed here
-            if (typeof $scope.data.tags != "undefined" && $scope.data.tags) { //can also be null
-                var tags = typeof $scope.data.tags == "string" ? $scope.data.tags.split(",") : $scope.data.tags;
-                for (var i = 0; i < tags.length; i++)
-                    $scope.tags.push({id: tags[i], text: tags[i]});
-            }
+//            $scope.tags = [];
+//// all of the next block is just to show how to push into the tags autocomplete/dropdown the list of available tags should be loaded this way instead,
+//// the model tags of the player are actually set properly from the ng-model of the tags directive and are not needed here
+//            if (typeof $scope.data.tags != "undefined" && $scope.data.tags) { //can also be null
+//                var tags = typeof $scope.data.tags == "string" ? $scope.data.tags.split(",") : $scope.data.tags;
+//                for (var i = 0; i < tags.length; i++)
+//                    $scope.tags.push({id: tags[i], text: tags[i]});
+//            }
 //registers the tags to be available to the directive
             menuSvc.registerAction('getTags', function() {
                 return $scope.tags;
