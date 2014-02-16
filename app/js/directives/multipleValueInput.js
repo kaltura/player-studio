96f615125918f386d82417a46088b4e2fb426a65
Nadav Sinai <ns@nadavsinai.com>
2014-02-16 16:49:17 +0200

    finish multipleInput

--- app/js/directives/multipleValueInput.js
+++ app/js/directives/multipleValueInput.js
@@ -3,17 +3,28 @@ var DirectivesModule = angular.module('KMC.directives');
 DirectivesModule.directive('multipleValueInput', function() {
     return {
         templateUrl: 'template/formcontrols/multipleValueInput.html',
+        replace: true,
         scope: {
             model: '=',
-            label:'@',
-            icon:'@'
+            label: '@',
+            icon: '@'
         }, controller: function($scope, $element, $attrs) {
             $scope.splitModel = [];
-            $scope.splitChar = $attrs(['splitChar']) | ';';
-            if ($scope.model) {
-                $scope.splitModel = $scope.model.split(splitChar);
+            if ($attrs['initvalue'] && !$scope.model) {
+                $scope.model = $attrs['initvalue'];
             }
-            $scope.add= function(){
+            $scope.remove = function(index) {
+                $scope.splitModel.splice(index, 1);
+            };
+            $scope.splitChar = $attrs['splitChar'] || ';';
+            if ($scope.model) {
+                $scope.splitModel = $scope.model.split($scope.splitChar);
+                if ($scope.splitModel[$scope.splitModel.length-1] == '') {
+                    $scope.splitModel.splice($scope.splitModel.length-1, 1);
+                }
+            }
+
+            $scope.add = function() {
                 $scope.splitModel.push('');
             };
         }, compile: function(tElem, tAttrs) {
@@ -21,15 +32,15 @@ DirectivesModule.directive('multipleValueInput', function() {
                 tElem.append('<hr/>');
             }
             return function($scope, $element, $attrs) {
-                $scope.$watchCollection('splitModel',function(newVal,oldVal){
-                    if (newVal===oldVal) return;
+                $scope.$watchCollection('splitModel', function(newVal, oldVal) {
+                    if (newVal === oldVal) return;
                     var str = '';
-                    angular.forEach(newVal,function(value){
-                        if ($.trim(value).length){
-                            str  += value+$scope.splitChar;
+                    angular.forEach(newVal, function(value) {
+                        if ($.trim(value).length) {
+                            str += value + $scope.splitChar;
                         }
                     });
-                    if (str!== $scope.model){ // to prevent unneeded digests
+                    if (str !== $scope.model) { // to prevent unneeded digests
                         $scope.model = str;
                     }
                 });
