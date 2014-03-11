c6ef43ca828303a48931dc24a988fa949e6d7732
Nadav Sinai <ns@nadavsinai.com>
2014-03-11 12:54:11 +0200

    initial commit  dynamic sections.

--- app/js/directives/modelSections.js
+++ app/js/directives/modelSections.js
@@ -1,12 +1,12 @@
 'use strict';
 var DirectivesModule = angular.module('KMC.directives');
-DirectivesModule.provider('sections', [ function() {
+DirectivesModule.provider('sections', [ function () {
     var templates = {
         dynamic: 'template/menu/dynamicSections.html',
         tabs: 'template/menu/tabs.html'
     };
-    this.$get = ['$compile', function($compile) {
-        return function(sectionType) {
+    this.$get = ['$compile', 'menuSvc', function ($compile, menuSvc) {
+        return function (sectionType) {
             return {
                 restrict: "AE",
                 replace: true,
@@ -14,32 +14,39 @@ DirectivesModule.provider('sections', [ function() {
                 templateUrl: templates[sectionType],
                 transclude: true,
                 scope: {heading: '@'},
-                controller: function($scope, $element, $attrs) {
+                controller: function ($scope, $element, $attrs) {
                     $scope.sections = {};
+                    var configData = menuSvc.getControlData($attrs.model);
+                    if (configData) {
+                        $scope.configData = configData.sections.sectionsConfig[$attrs.section];
+                    }
                     $scope.dynamic = (sectionType === 'dynamic') ? true : false;
+                    if ($scope.dynamic) {
+                        $scope.sections = [];
+                    }
+
                 },
-                compile: function(tElem, tAttrs) {
-
-                    return function($scope, $element, $attrs, sectionsCntrl) {
-//                        var tabset = $compile($element.find('div[tabset]'))($scope);
-//                        transcludeFn(function(clone) {
-//                            tabset.append(clone);
-//                        });
-                        if (sectionType == 'tabs') {
-                            $scope.tabset = {heading: $attrs['heading']};
-                        }
-                        if (sectionType == 'dynamic') {
-
-                        }
-                    };
+                link: function ($scope, $element, $attrs, controller, transclude) {
+                    if (sectionType == 'tabs') {
+                        $scope.tabset = {heading: $attrs['heading']};
+                    }
+                    if (sectionType == 'dynamic') {
+                        transclude($scope, function (clone) {
+                            $scope.template = clone;
+                            $scope.sections.push({template:clone});
+                        });
+                        window.dScope = $scope;
+                    }
                 }
             };
         };
-    }];
-}]);
-DirectivesModule.directive('kaDynamicSection', ['sections', function(sections) {
+    }]
+    ;
+}])
+;
+DirectivesModule.directive('kaDynamicSection', ['sections', function (sections) {
     return sections('dynamic');
 }]);
-DirectivesModule.directive('kaTabs', ['sections', function(sections) {
+DirectivesModule.directive('kaTabs', ['sections', function (sections) {
     return sections('tabs');
 }]);
