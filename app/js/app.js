f101ea01e0dbe73f7dbf29539d86c6ec00e3ea6e
Nadav Sinai <ns@nadavsinai.com>
2014-03-25 11:57:05 +0200

    ie9 compatibity

--- app/js/app.js
+++ app/js/app.js
@@ -255,4 +255,14 @@ KMCModule.config(['$routeProvider', '$locationProvider', '$httpProvider', '$tool
             debug = false;
         }
     });
+    if (Function.prototype.name === undefined && Object.defineProperty !== undefined) { // patch IE9's function prototype
+        Object.defineProperty(Function.prototype, 'name', {
+            get: function() {
+                var funcNameRegex = /function\s([^(]{1,})\(/;
+                var results = (funcNameRegex).exec((this).toString());
+                return (results && results.length > 1) ? results[1].trim() : "";
+            },
+            set: function(value) {}
+        });
+    }
 });
