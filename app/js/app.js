6a09d4b98b1d9a71159c8075a5808f6e7c89918b
amirch1 <amir.chervinsky@kaltura.com>
2014-04-01 16:05:44 +0300

    #FEC-1168 #comment fixed kmc.vars referenced to parent window

--- app/js/app.js
+++ app/js/app.js
@@ -255,10 +255,11 @@ KMCModule.config(['$routeProvider', '$locationProvider', '$httpProvider', '$tool
             debug = false;
         }
     });
-    if (window.kmc && window.kmc.vars.studio.showFlashStudio === false){
+	var kmc = window.parent.kmc;
+    if (kmc && kmc.vars.studio.showFlashStudio === false){
         $("#flashStudioBtn").hide();
     }
-    if (window.kmc && window.kmc.vars.studio.showHTMLStudio === false){
+    if (kmc && kmc.vars.studio.showHTMLStudio === false){
         $("#htmlStudioBtn").hide();
     }
 
