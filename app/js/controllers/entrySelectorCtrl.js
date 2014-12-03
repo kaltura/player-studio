ad7b187b8cfa3b3058827f171ffa2b50d3136331
amirch1 <amir.chervinsky@kaltura.com>
2014-12-03 10:02:57 +0200

    #SUP-3193 #time 2h #comment added entrySelector controller to replace ID with the corresponding label

---
+++ app/js/controllers/entrySelectorCtrl.js
@@ -0,0 +1,13 @@
+KMCMenu.controller('entrySelectorCtrl', ['$scope',
+	function ($scope) {
+		$scope.getLabel = function(id, configObject){
+			var searchArr = configObject == "playlistSelectBox" ? $scope.userPlaylists : $scope.userEntries;
+			for (var i = 0; i < searchArr.length; i++){
+				if (searchArr[i].id === id){
+					return searchArr[i].text;
+				}
+			}
+			return id;
+		}
+	}
+]);
