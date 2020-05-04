0f562a1719d85507f865d65cc58d0e9bc6f6bf22
Yair Ansbacher <yair.ansbacher@kaltura.com>
2020-05-04 13:48:44 +0300

    fix(10020): if some language duplicated in uiconf, in studio default language list will be empty (#82)
    remove duplicate languages

--- app/js/controllers/EditCtrl.js
+++ app/js/controllers/EditCtrl.js
@@ -16,7 +16,7 @@ KMCMenu.controller('EditCtrl', ['$scope','$http', '$timeout','PlayerData','Playe
 	try {
 		var confVarsObj = JSON.parse($scope.playerData.confVars);
 		if (confVarsObj) {
-			$scope.playerData['playerLangCodes'] = confVarsObj.langs || ['en'];
+			$scope.playerData['playerLangCodes'] = $.unique(confVarsObj.langs) || ['en'];
 			var versionsObj = confVarsObj.versions || confVarsObj;
 			PlayerService.OvpOrOtt = versionsObj[PlayerService.KALTURA_PLAYER] ? PlayerService.OVP : PlayerService.OTT;
 			var playerName = versionsObj[PlayerService.KALTURA_PLAYER] ? PlayerService.KALTURA_PLAYER : PlayerService.KALTURA_PLAYER_OTT;
