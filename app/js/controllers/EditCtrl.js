42948fa02983ae3e0d81c5af20415d29064084d1
yair.ansbacher <yairans@gmail.com>
2020-05-04 13:52:47 +0300

    bump version to v3.5.0.rc4

--- app/js/controllers/EditCtrl.js
+++ app/js/controllers/EditCtrl.js
@@ -415,7 +415,7 @@ KMCMenu.controller('EditCtrl', ['$scope','$http', '$timeout','PlayerData','Playe
 	    if (property.model === "playerLangCodes"){
 		    $scope.playerLangCodesChanged = true;
 		    if ($scope.playerData.playerLangCodes.length === 0 && property.initvalue[0] !== 'en') {
-		    	// move from empty (english by default) to non-english
+				// move from empty (english by default) to non-english
 			    window.KalturaPlayer = null;
 		    }
 		    $scope.playerData.playerLangCodes = property.initvalue || [];
