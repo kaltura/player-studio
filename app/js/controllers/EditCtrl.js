795fa4ff4a16035a7ee17d3261e2088aef3103eb
Yair Ansbacher <yair.ansbacher@kaltura.com>
2021-01-12 16:00:37 +0200

    feat(FEC-10868): Timeline Scrubber Studio Configuration (#100)
    Solves [FEC-10868](https://kaltura.atlassian.net/browse/FEC-10868) and [FEC-10550](https://kaltura.atlassian.net/browse/FEC-10550)

--- app/js/controllers/EditCtrl.js
+++ app/js/controllers/EditCtrl.js
@@ -11,6 +11,8 @@ KMCMenu.controller('EditCtrl', ['$scope','$http', '$timeout','PlayerData','Playe
 	$scope.aspectRatio = playerRatio == (9/16) ? "wide" : playerRatio == (3/4) ? "narrow" : "custom";  // set aspect ratio to wide screen
 	$scope.newPlayer = !$routeParams.id;            // New player flag
 	$scope.menuOpen = true;
+	$scope.dependencyPlugins = {};
+	$scope.dependencyExternals = {};
 
 	try {
 		var confVarsObj = JSON.parse($scope.playerData.confVars);
@@ -273,6 +275,9 @@ KMCMenu.controller('EditCtrl', ['$scope','$http', '$timeout','PlayerData','Playe
 		    // since we are getting the event before the value is changed - enabled means that the plugin is going to be disabled - remove validation
 		    $scope.removeValidation(plugin);
 		    delete $scope.playerData.config.plugins[plugin.model]; // remove the plugin from the player data
+		    for (var i = 0; i < plugin.properties.length; i++) {
+			    $scope.handleDependencies(plugin.properties[i].dependencies, plugin.properties[i].model)
+		    }
 	    }else{
 			if (plugin.componentName) {
 			    window.KalturaPlayer = null;
@@ -716,6 +721,7 @@ KMCMenu.controller('EditCtrl', ['$scope','$http', '$timeout','PlayerData','Playe
 					}
 			}
 		}
+		$scope.removeUnusedDependencies();
 	};
 
 	$scope.setPluginData = function(plugin){
@@ -748,8 +754,9 @@ KMCMenu.controller('EditCtrl', ['$scope','$http', '$timeout','PlayerData','Playe
 				var prop = objArr[j];
 				if (j == objArr.length-1 && data.initvalue !== undefined){  // last object in model path - this is the value property
 					pData[prop] = data.filter ? $scope.setFilter(data.initvalue, data.filter) : data.initvalue; // set the data in this property
+					$scope.handleDependencies(data.dependencies, data.model, data.initvalue);
 				}else{
-					if (j == objArr.length-2 && !pData[prop]){ // object path doesn't exist - create is (add plugin that was enabled)
+					if (j <= objArr.length-2 && !pData[prop]){ // object path doesn't exist - create is (add plugin that was enabled)
 						pData[prop] = data.custom ? {'custom':true, 'enabled':true} : {'enabled':true};
 					}
 					if (pData[prop]) {
@@ -818,6 +825,53 @@ KMCMenu.controller('EditCtrl', ['$scope','$http', '$timeout','PlayerData','Playe
 			return data || [];
 		}
 		return data;
+	};
+
+	$scope.handleDependencies = function(propDependencies, propModel, propValue) {
+		if (propDependencies) {
+			for (var i = 0; i < propDependencies.length; i++) {
+				var dependency = propDependencies[i];
+				if (propValue && propValue === dependency.condition) {
+					if (dependency.componentName) {
+						if (!($scope.playerData.plugins[dependency.plugin] || $scope.dependencyExternals[dependency.componentName])) {
+							window.KalturaPlayer = null;
+						}
+						if (dependency.pluginConfig) {
+							$scope.playerData.plugins[dependency.plugin] = {componentName: dependency.componentName}
+						} else {
+							$scope.playerData.externals[dependency.componentName] = {active: true};
+							$scope.dependencyExternals[dependency.componentName] = $scope.dependencyExternals[dependency.componentName] || {};
+							$scope.dependencyExternals[dependency.componentName][propModel] = true;
+						}
+					}
+					if (dependency.pluginConfig) {
+						$scope.playerData.config.plugins[dependency.plugin] = dependency.pluginConfig;
+						$scope.dependencyPlugins[dependency.plugin] = $scope.dependencyPlugins[dependency.plugin] || {};
+						$scope.dependencyPlugins[dependency.plugin][propModel] = true;
+					}
+				} else {
+					if ($scope.dependencyPlugins[dependency.plugin]) {
+						delete $scope.dependencyPlugins[dependency.plugin][propModel];
+					}
+					if ($scope.dependencyExternals[dependency.componentName]) {
+						delete $scope.dependencyExternals[dependency.componentName][propModel];
+					}
+				}
+			}
+		}
+	};
+
+	$scope.removeUnusedDependencies = function () {
+		for (var plugin in $scope.dependencyPlugins) {
+			if ($.isEmptyObject($scope.dependencyPlugins[plugin])) {
+				delete $scope.playerData.config.plugins[plugin];
+			}
+		}
+		for (var external in $scope.dependencyExternals) {
+			if ($.isEmptyObject($scope.dependencyExternals[external])) {
+				$scope.playerData.externals[external] = {active: false};
+			}
+		}
 	};
 
 	$scope.backToList = function(){
