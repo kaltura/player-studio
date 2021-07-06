263dbfabe86a901f61ee6a5a74edcf37a26f73e7
yuval.keidar <keidaryu@gmail.com>
2021-07-06 12:10:50 +0300

    build: test grunt

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
 set -ev
-npm install grunt
+npm install -g grunt
 if [ "${TRAVIS_MODE}" = "release" ]; then
   echo "Building..."
   TAG=$TRAVIS_TAG yarn run build
   echo "Finish building"
 else
-	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
-	exit 1
+  echo "Building..."
+  TAG=$TRAVIS_TAG yarn run build
+  echo "Finish building"
+#	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
+#	exit 1
 fi
