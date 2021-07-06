535660324dd26ad12997e1049f429656dccbfd5b
yuval.keidar <keidaryu@gmail.com>
2021-07-06 13:20:38 +0300

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,14 +2,14 @@
 read -p "Enter Tag Number without prefix v: "  tag
 
 PREV_TAG=$(git describe --tags)
-TAG_WITHOUT_V="${PREV_TAG#v}"
+PREV_TAG_WITHOUT_V="${PREV_TAG#v}"
 NEW_TAG="v$tag"
 
 echo "creating $NEW_TAG!"
 echo "previous tag $PREV_TAG!"
 
-sed -iE "s/$TAG_WITHOUT_V/$tag/g" ./app/studio.ini
-sed -iE "s/$TAG_WITHOUT_V/$tag/g" ./app/dist_src/studio.ini
+sed -iE "s/$PREV_TAG_WITHOUT_V/$tag/g" ./app/studio.ini
+sed -iE "s/$PREV_TAG_WITHOUT_V/$tag/g" ./app/dist_src/studio.ini
 rm ./app/studio.iniE
 rm ./app/dist_src/studio.iniE
 
