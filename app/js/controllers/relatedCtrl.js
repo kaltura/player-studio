0c522f094b3f1ec99e0c3bb9a402f417a1ec6611
JonathanTGold <62672270+JonathanTGold@users.noreply.github.com>
2022-03-08 13:41:42 +0200

    feat(FEC-11590): Related grid Studio requirements (#120)
    Add Related-Plugin configuration to the V3-Studio
    And add backward compatibility support for ES6 syntax on the bundle building process
    
    solves: FEC-11590

--- app/js/controllers/relatedCtrl.js
+++ app/js/controllers/relatedCtrl.js
@@ -1,15 +1,19 @@
 KMCMenu.controller('relatedCtrl', ['$scope',
 	function ($scope) {
 		$scope.relatedOption = "relatedToEntry";
-		$scope.entryList = "";
+		$scope.entryList = '';
 		$scope.playlistId = {id:'' ,text:''};
+		if(!("related" in $scope.playerData.config.plugins)) {
+			$scope.playerData.config.plugins["related"] = {disable: true};
+			$scope.playerData.config.plugins["related"].useContext = true;
+		}
 
 		// init radio buttons according to selected related entries source
-		if ($scope.playerData.config.player.plugins && $scope.playerData.config.player.plugins["related"]) {
-			var data = $scope.playerData.config.player.plugins["related"];
-			if (data.entryList && data.entryList !== ""){
+		if ($scope.playerData.config.plugins["related"].enabled) {
+			var data = $scope.playerData.config.plugins["related"];
+			if (Array.isArray(data.entryList) && data.entryList.length > 0){
 				$scope.relatedOption = "entryList";
-				$scope.entryList = data.entryList;
+				$scope.entryList = data.entryList.map(mediaInfo => mediaInfo.entryId).join(',');
 			}
 			if (data.playlistId && data.playlistId !== ""){
 				$scope.relatedOption = "playlistId";
@@ -20,33 +24,33 @@ KMCMenu.controller('relatedCtrl', ['$scope',
 		// update entries list
 		$scope.entryListChange = function(){
 			// unselect playlist ID
-			$scope.playerData.config.player.plugins["related"].playlistId = null;
-			$scope.playlistId = {id:'',text:''};
+			$scope.playerData.config.plugins["related"].playlistId = null;
+			$scope.playerData.config.plugins["related"].useContext = false;
 			// set entries list
-			$scope.playerData.config.player.plugins["related"].entryList = $scope.entryList;
+			$scope.playerData.config.plugins["related"].entryList = $scope.entryList.split(',').map(id => ({entryId: id.trim()}));
 		};
 
 		// update playlist ID
 		$scope.playlistIdChange = function(){
 			// unselect entries list
-			$scope.playerData.config.player.plugins["related"].entryList = null;
-			$scope.entryList = "";
+			$scope.playerData.config.plugins["related"].entryList = [];
+			$scope.playerData.config.plugins["related"].useContext = false;
 			// set playlist ID
-			$scope.playerData.config.player.plugins["related"].playlistId = $scope.playlistId.id;
+			$scope.playerData.config.plugins["related"].playlistId = $scope.playlistId.id;
 		};
 
 		// clear selection of unselected radio button potions
 		$scope.relatedSelected = function(){
-			$scope.playerData.config.player.plugins["related"].playlistId = undefined;
-			$scope.playerData.config.player.plugins["related"].entryList = null;
-			$scope.playlistId = {id:'',text:''};
-			$scope.entryList = "";
+			$scope.playerData.config.plugins["related"].playlistId = null;
+			$scope.playerData.config.plugins["related"].entryList = [];
+			$scope.playerData.config.plugins["related"].useContext = true;
+
 			$scope.propertyChanged("related", true);
 		};
 
 		// required for the playlist selector
 		$scope.getLabel = function(id, configObject){
-			var searchArr = configObject == "playlistSelectBox" ? $scope.userPlaylists : $scope.userEntries;
+			var searchArr = configObject === "playlistSelectBox" ? $scope.userPlaylists : $scope.userEntries;
 			for (var i = 0; i < searchArr.length; i++){
 				if (searchArr[i].id === id){
 					return searchArr[i].text;
@@ -54,6 +58,5 @@ KMCMenu.controller('relatedCtrl', ['$scope',
 			}
 			return id;
 		};
-
 	}
 ]);
