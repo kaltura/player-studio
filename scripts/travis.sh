8ce5903cbd23a5207589fdb9f9411e4e1f5c21ed
JonathanTGold <jonathan.gold@86@gmail.com>
2022-03-09 18:16:17 +0200

    travis edit

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,12 +2,14 @@
 # https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
 set -ev
 npm install -g grunt
-#if [ "${TRAVIS_MODE}" = "release" ]; then
+yarn install
+if [ "${TRAVIS_MODE}" = "release" ]; then
   echo "Building..."
-#  TAG=$TRAVIS_TAG yarn run build
-  yarn run build
+  TAG=$TRAVIS_TAG yarn run build
   echo "Finish building"
-#else
-#	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
-#	exit 1
-#fi
+elif [ "${TRAVIS_MODE}" = "build" ]; then
+	 yarn run build
+else
+	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
+	exit 1
+fi
