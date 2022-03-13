429b23db4a25f9272c9673961d17d3a604ec947f
JonathanTGold <jonathan.gold@86@gmail.com>
2022-03-13 12:32:43 +0200

    fix(FEC-12051): [Studio - v3]: Automate release process

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-read -p "Enter Tag Number without prefix v: "  tag
 
-NEW_TAG="v$tag"
-echo "creating $NEW_TAG!"
+NEW_TAG=$(git describe --abbrev=0)
 
+echo "extracting the new tag - $NEW_TAG"
 sed -e "s/{{tagVersion}}/$tag/g" ./template_studio.ini > ./app/dist_src/studio.ini
 
-git commit -a -m "bump version to $NEW_TAG"
-git tag $NEW_TAG
+git commit --amend --no-edit
