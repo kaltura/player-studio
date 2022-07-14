9f1724d02bf8428f9fd9e448bb5c92136ee4ec15
JonathanTGold <62672270+JonathanTGold@users.noreply.github.com>
2022-07-14 14:09:02 +0200

    feat(FEC-12364): Add support for GTM Plugin in V3 studio (#128)
    Add support for GTM Plugin in V3 studio
    
    related pr: kaltura/playkit-js-google-tag-manager#1

---
+++ app/js/controllers/gtmPluginCtrl.js
@@ -0,0 +1,26 @@
+KMCMenu.controller('gtmPluginCtrl', ['$scope',
+	function ($scope) {
+		$scope.customListOfCustomEvents = '';
+
+		if(!("googleTagManager" in $scope.playerData.config.plugins)) {
+			$scope.playerData.config.plugins["googleTagManager"] = {disable: true};
+		}
+
+		if ($scope.playerData.config.plugins["googleTagManager"].enabled) {
+			var data = $scope.playerData.config.plugins["googleTagManager"];
+			if (Array.isArray(data.customEventsTracking && data.customEventsTracking.custom) && data.customEventsTracking.custom.length > 0){
+				$scope.customListOfCustomEvents = data.customEventsTracking.custom.join(',');
+			} else if (!('customEventsTracking' in $scope.playerData.config.plugins["googleTagManager"])) {
+				$scope.playerData.config.plugins["googleTagManager"].customEventsTracking = {custom: []};
+			} else {
+				$scope.playerData.config.plugins["googleTagManager"].customEventsTracking.custom = '';
+			}
+		}
+
+		$scope.customListOfCustomEventsChange = function(){
+			$scope.playerData.config.plugins["googleTagManager"].customEventsTracking.custom = $scope.customListOfCustomEvents
+				.replace(/\s/g,'')
+				.split(',');
+		};
+	}
+]);
