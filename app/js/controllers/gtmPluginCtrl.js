d328e9d6b368c9df20069ae5dd43f47d8889690b
JonathanTGold <62672270+JonathanTGold@users.noreply.github.com>
2022-07-21 17:56:46 +0200

    fix(FEC-12364): Add input validation and fix bug custom is not updated first time plugin set (#132)
    * fix(FEC-12364): Add validation and fix bug

--- app/js/controllers/gtmPluginCtrl.js
+++ app/js/controllers/gtmPluginCtrl.js
@@ -2,22 +2,23 @@ KMCMenu.controller('gtmPluginCtrl', ['$scope',
 	function ($scope) {
 		$scope.customListOfCustomEvents = '';
 
+		// Plugin doesn't exist in this uiconf
 		if(!("googleTagManager" in $scope.playerData.config.plugins)) {
 			$scope.playerData.config.plugins["googleTagManager"] = {disable: true};
-		}
-
-		if ($scope.playerData.config.plugins["googleTagManager"].enabled) {
+			$scope.playerData.config.plugins["googleTagManager"].customEventsTracking = {custom:[]};
+		// Plugin enabled in this uiconf
+		} else if ($scope.playerData.config.plugins["googleTagManager"].enabled) {
 			var data = $scope.playerData.config.plugins["googleTagManager"];
 			if (Array.isArray(data.customEventsTracking && data.customEventsTracking.custom) && data.customEventsTracking.custom.length > 0){
 				$scope.customListOfCustomEvents = data.customEventsTracking.custom.join(',');
-			} else {
-				$scope.playerData.config.plugins["googleTagManager"] = {customEventsTracking: {custom:[]}};
 			}
 		}
 
 		$scope.customListOfCustomEventsChange = function(){
 			$scope.playerData.config.plugins["googleTagManager"].customEventsTracking.custom = $scope.customListOfCustomEvents
 				.replace(/\s/g,'')
+				.replace(/^,|,$/g,'')
+				.replace(/,,+/g,',')
 				.split(',');
 		};
 	}
