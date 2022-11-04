727d860460ffbf269ab15e40b09c2522c13ba15a
Vladimir Poleh <vladimir.poleh@kaltura.com>
2022-11-04 20:21:22 +0300

    Update Travis build.

---
+++ scripts/travis.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
+set -ev
+npm install -g grunt
+if [ "${TRAVIS_MODE}" = "release" ]; then
+  echo "Building..."
+  TAG=$TRAVIS_TAG yarn run build
+  echo "Finish building"
+elif [ "${TRAVIS_MODE}" = "build" ]; then
+	 yarn run build
+else
+	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
+	exit 1
+fi
